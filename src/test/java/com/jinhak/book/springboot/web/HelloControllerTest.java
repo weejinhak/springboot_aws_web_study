100d086d4a4f95483ab9d0d324cd4c2e315bb111
weejinhak <wlsgkr91@gmail.com>
2022-06-14 23:56:11 +0900

    20200614 Study
    스프링부트와 AWS로 혼자 구현하는 웹서비스
    1. Gradle -> Spring Boot Project 변환
    2. 테스트코드 소개 및 작성
    3. 롬복 소개 및 전환

---
+++ src/test/java/com/jinhak/book/springboot/web/HelloControllerTest.java
@@ -0,0 +1,49 @@
+package com.jinhak.book.springboot.web;
+
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = HelloController.class)
+public class HelloControllerTest {
+
+	@Autowired
+	private MockMvc mvc;
+
+	@Test
+	public void hello가_리턴된다() throws Exception {
+		String hello = "hello";
+
+		mvc.perform(MockMvcRequestBuilders.get("/hello"))
+			.andExpect(status().isOk())
+			.andExpect(content().string(hello));
+
+	}
+
+	@Test
+	public void helloDto가_리턴된다() throws Exception {
+		String name = "hello";
+		int amount = 1000;
+
+		mvc.perform(
+				MockMvcRequestBuilders.get("/hello/dto")
+					.param("name", name)
+					.param("amount", String.valueOf(amount)))
+			.andExpect(status().isOk())
+			.andExpect((ResultMatcher) jsonPath("$.name", is(name)))
+			.andExpect((ResultMatcher) jsonPath("$.amount", is(amount)));
+
+	}
+}
